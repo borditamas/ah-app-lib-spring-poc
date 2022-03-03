8b53b5dea012c22756912a1e0d464ffbff33cbb3
Tamas Bordi <borditamas@gmail.com>
2022-03-03 14:48:01 +0100

    library structure (not finished)

--- src/main/java/ai/aitia/arrowhead/application/spring/networking/http/decoder/JSONDecoder.java
+++ src/main/java/ai/aitia/arrowhead/application/spring/networking/http/decoder/JSONDecoder.java
@@ -1,19 +1,60 @@
 package ai.aitia.arrowhead.application.spring.networking.http.decoder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ai.aitia.arrowhead.application.common.networking.decoder.MediaType;
 import ai.aitia.arrowhead.application.common.networking.decoder.PayloadDecoder;
+import ai.aitia.arrowhead.application.common.networking.decoder.exception.PayloadDecodingException;
+import ai.aitia.arrowhead.application.common.verification.Ensure;
 
 public class JSONDecoder implements PayloadDecoder {
 
+	//=================================================================================================
+	// members
+	
+	private final ObjectMapper mapper = new ObjectMapper();
+	
+	//=================================================================================================
+	// methods
+
+	//-------------------------------------------------------------------------------------------------
 	@Override
-	public <T> T decode(final MediaType media, byte[] payload, Class<T> type) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public <T> T decode(final MediaType media, final byte[] payload, final Class<T> type) throws Exception {
+		Ensure.notNull(media, "media is null");
+		Ensure.notEmpty(payload, "payload is empty");
+		Ensure.notNull(type, "type is null");
+		
+		switch (media) {
+		case EMPTY:
+			return null;
+			
+		case TEXT:
+		case JSON:
+			return mapper.readValue(payload, type);
+
+		default:
+			throw new PayloadDecodingException("Unknown media type: " + media.name());
+		}
 	}
 
+	//-------------------------------------------------------------------------------------------------
 	@Override
-	public <T> T decode(final MediaType media, String payload, Class<T> type) throws Exception {
-		return null;
+	public <T> T decode(final MediaType media, final String payload, final Class<T> type) throws Exception {
+		Ensure.notNull(media, "media is null");
+		Ensure.notEmpty(payload, "payload is empty");
+		Ensure.notNull(type, "type is null");
+		
+		switch (media) {
+		case EMPTY:
+			return null;
+			
+		case TEXT:
+		case JSON:
+			return mapper.readValue(payload, type);
+
+		default:
+			throw new PayloadDecodingException("Unknown media type" + media.name());
+		}
 	}
 
 }
